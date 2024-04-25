5bd9e41abe89710cf8dcd7714bac3c9433f70b15
anushri26 <97968818+anushri26@users.noreply.github.com>
2024-04-25 18:14:07 +0530

    Update diff.py

--- tdlpro/data/diff.py
+++ tdlpro/data/diff.py
@@ -9,15 +9,15 @@ folder2_path = "output-images"
 image1_path = os.path.join(folder1_path, "mosaic.jpg")
 image2_path = os.path.join(folder2_path, "trained.jpg")  
 
-# Load the images
+# Loading the images
 image1 = torchvision.io.read_image(image1_path).unsqueeze(0)
 image2 = torchvision.io.read_image(image2_path).unsqueeze(0)
 
-# Resize the images to the same dimensions
+# Resizing the images to the same dimensions
 image1 = TF.resize(image1, (256, 256))
 image2 = TF.resize(image2, (256, 256))
 
-# Create the LPIPS model
+# Creating the LPIPS model
 loss_fn = lpips.LPIPS(net="vgg")
 
 # Compute the LPIPS distance
