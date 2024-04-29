13faadfddcea8c927a953ea282e1de3ab53b2050
Anurag Vadhyar <92390229+anuragvadhyar@users.noreply.github.com>
2024-04-29 16:43:32 +0530

    Update perceptual_loss_net.py

--- tdlpro/models/definitions/perceptual_loss_net.py
+++ tdlpro/models/definitions/perceptual_loss_net.py
@@ -8,7 +8,6 @@ from torchvision import models
 class Vgg16(torch.nn.Module):
     def __init__(self, requires_grad=False, show_progress=False):
         super().__init__()
-        # Keeping eval() mode only for consistency - it only affects BatchNorm and Dropout both of which we won't use
         vgg16 = models.vgg16(pretrained=True, progress=show_progress).eval()
         vgg_pretrained_features = vgg16.features
         self.layer_names = ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3']
@@ -26,7 +25,6 @@ class Vgg16(torch.nn.Module):
         for x in range(16, 23):
             self.slice4.add_module(str(x), vgg_pretrained_features[x])
 
-        # Set these to False so that PyTorch won't be including them in it's autograd engine - eating up precious memory
         if not requires_grad:
             for param in self.parameters():
                 param.requires_grad = False
